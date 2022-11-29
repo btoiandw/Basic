5d48af15ccb5b261c41336d03071f928bb7b902a
my git btoiandw <bbdbiw2544@gmail.com>
2022-11-29 11:20:51 +0700

    basic C# first commit

---
+++ Basic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi!!");
+            Console.WriteLine("Hello World");
+            Console.ReadLine(); //รอรับ enter เพื่อปิดโปรแกรม
+        }
+    }
+}
